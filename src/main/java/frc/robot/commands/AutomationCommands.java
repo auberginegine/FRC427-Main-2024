c906d1fa6c551340a1a13caf80617bd8f9e45880
RemiIqbal <70063709+RemiIqbal@users.noreply.github.com>
2024-02-09 15:41:26 -0500

    Commented Automation commands

--- src/main/java/frc/robot/commands/AutomationCommands.java
+++ src/main/java/frc/robot/commands/AutomationCommands.java
@@ -19,40 +19,57 @@ import frc.robot.subsystems.intake.commands.OuttakeToSpeaker;
 import frc.robot.subsystems.leds.Led;
 
 public class AutomationCommands {
-    
+  
+  //Create autoIntakeCOmmand Command
   public static Command autoIntakeCommand() {
     return Commands.sequence(
+      //Set Leds to Intake
         Commands.runOnce(() -> Led.getInstance().isIntaking = true),
+        //Go to ground and intake
         new GoToGround(Arm.getInstance()), 
         new IntakeFromGround(Intake.getInstance())
     ).finallyDo(() -> {
+      //Reset arm to travel and reset Leds
       Arm.getInstance().goToAngle(Constants.ArmConstants.kTravelPosition);
       Commands.runOnce(() -> Led.getInstance().isIntaking = false);
     });
   }
 
+  //Create pathFindToSpeaker Command
   public static Command pathFindToSpeaker() {
+    //Sets leds
     return Commands.runOnce(() -> Led.getInstance().isMovingToSpeaker = true).andThen(
       Commands.defer(() -> MoveToSpeaker.goToSpeaker(), Set.of(Drivetrain.getInstance()))).finallyDo(() -> {
+        //Set leds off 
         Led.getInstance().isMovingToSpeaker = false;
       }); 
   }
 
+  //Create pathFindToSpeakerAndScore Command
   public static Command pathFindToAmp() {
+    //Set Leds
     return Commands.runOnce(() -> Led.getInstance().isMovingToAmp = true).andThen(
+      //Go To Amp
       Commands.defer(() -> MoveToAmp.goToAmp(), Set.of(Drivetrain.getInstance()))).finallyDo(() -> {
+        //Turn specific Leds off
         Led.getInstance().isMovingToAmp = false;
       }); 
   }
 
+  //Create pathFindToSpeakerAndScore Command
   public static Command pathFindToSpeakerAndScore(Arm arm, Intake intake) {
+    //Run Go to speaker and outtake to speaker
+
     return pathFindToSpeaker().alongWith(new GoToSpeaker(arm)).andThen(new OuttakeToSpeaker(intake)).finallyDo(() -> {
+      //When done, arm goes to travel position
       Arm.getInstance().goToAngle(Constants.ArmConstants.kTravelPosition);
     }); 
   }
-
+  //Create pathFindToAmpAnndScore Command
   public static Command pathFindToAmpAndScore(Arm arm, Intake intake) {
+    //Run pathFindToAmp
     return pathFindToAmp().alongWith(new GoToAmp(arm)).andThen(new OuttakeToAmp(intake)).finallyDo(() -> {
+      //When done, arm goes to travel position
       Arm.getInstance().goToAngle(Constants.ArmConstants.kTravelPosition);
     });
   }
@@ -62,10 +79,14 @@ public class AutomationCommands {
     return Commands.none(); 
   }
 
+
+  //Create ShootFromAnywhere Commands
   public static Command shootFromAnywhere() {
+    //When command runs set Leds to true, and then run Shoot Anywhere with Drivetrain/Arm/Intake
     return Commands.runOnce(() -> Led.getInstance().isShooting = true).andThen(Commands.defer(
       () -> ShootAnywhere.shootAnywhere(Drivetrain.getInstance(), Arm.getInstance(), Intake.getInstance()), 
     Set.of(Drivetrain.getInstance(), Arm.getInstance(), Intake.getInstance()))).finallyDo(() -> {
+      //Set Is Shooting to False
       Led.getInstance().isShooting = false;
     }); 
   }
