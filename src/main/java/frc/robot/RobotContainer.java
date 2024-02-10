30c5024068b0778ea2465ec8d9111cacc1afa7c0
BananasAmIRite <jasony1230@gmail.com>
2024-02-09 19:01:23 -0500

    added a hold rev stop shoot option for speaker shooting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,6 +33,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -144,8 +145,20 @@ public class RobotContainer {
         intake.stopShoot();
       }));
 
-    manipulatorController.leftBumper().and(() -> arm.getArmControlState() == ArmControlState.SPEAKER)
-      .whileTrue(new OuttakeToSpeaker(intake));
+      // TODO: see which one is better
+      // -- hold a button that revs up and outtakes
+    // manipulatorController.leftBumper().and(() -> arm.getArmControlState() == ArmControlState.SPEAKER)
+    //   .whileTrue(new OuttakeToSpeaker(intake));
+
+      // -- hold a button to rev up, outtakes after release
+      manipulatorController.leftBumper().and(() -> arm.getArmControlState() == ArmControlState.SPEAKER)
+      .whileTrue(new SetShooterSpeed(intake, 1))
+      .onFalse(
+        new SetSuckerIntakeSpeed(intake, 1)
+        .andThen(new WaitCommand(0.5))
+        .andThen(new SetShooterSpeed(intake, 0))
+        .andThen(new SetSuckerIntakeSpeed(intake, 0))
+      );
       
      // intake
      manipulatorController.leftBumper(). and(() -> arm.getArmControlState() == ArmControlState.GROUND)
