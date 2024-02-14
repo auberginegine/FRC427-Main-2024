018054c256c3cb5bd7dc7264601525d22a857d5d
auberginegine <8280187@philasd.org>
2024-02-14 16:42:07 -0500

    added getAngle to auto han d

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -318,14 +318,37 @@ public final class Constants {
     public static final Pose2d ampRed = new Pose2d(14.62, 7.75, Rotation2d.fromDegrees(-90));
   }
   public static final class AutoHang {
-    public static final Pose2d topLeft1 = new Pose2d(3.30, 7.00, Rotation2d.fromDegrees(0));
-    public static final Pose2d bottomRight1 = new Pose2d(5.50, 5.50, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueTopLeft1 = new Pose2d(2.05, 5.93, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueBottomRight1 = new Pose2d(5.48, 5.55, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueTopRight1 = new Pose2d(4.56, 6.77, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueBottomLeft1 = new Pose2d(3.15, 4.50, Rotation2d.fromDegrees(0));
 
-    public static final Pose2d topLeft2 = new Pose2d(6.20, 5.50, Rotation2d.fromDegrees(0));
-    public static final Pose2d bottomRight2 = new Pose2d(7.90, 3.33, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueTopLeft2 = new Pose2d(6.20, 5.50, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueBottomRight2 = new Pose2d(7.90, 3.33, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueTopRight2 = new Pose2d(7.90, 5.50, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueBottomLeft2 = new Pose2d(6.20, 3.33, Rotation2d.fromDegrees(0));
 
-    public static final Pose2d topLeft3 = new Pose2d(3.30, 3.50, Rotation2d.fromDegrees(0));
-    public static final Pose2d bottomRight3 = new Pose2d(4.95, 2.50, Rotation2d.fromDegrees(0));
+
+    public static final Pose2d blueTopLeft3 = new Pose2d(3.23, 3.92, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueBottomRight3 = new Pose2d(4.40, 1.56, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueTopRight3 = new Pose2d(5.32, 2.66, Rotation2d.fromDegrees(0));
+    public static final Pose2d blueBottomLeft3 = new Pose2d(2.21, 2.79, Rotation2d.fromDegrees(0));
+
+    public static final Pose2d redTopRight1 = new Pose2d(14.61, 5.93, Rotation2d.fromDegrees(0));
+    public static final Pose2d redBottomRight1 = new Pose2d(13.30, 4.30, Rotation2d.fromDegrees(0));
+    public static final Pose2d redTopLeft1 = new Pose2d(11.99, 6.77, Rotation2d.fromDegrees(0));
+    public static final Pose2d redBottomLeft1 = new Pose2d(11.22, 5.55, Rotation2d.fromDegrees(0));
+
+    public static final Pose2d redTopLeft2 = new Pose2d(9.20, 5.31, Rotation2d.fromDegrees(0));
+    public static final Pose2d redBottomRight2 = new Pose2d(10.70, 2.80, Rotation2d.fromDegrees(0));
+    public static final Pose2d redTopRight2 = new Pose2d(10.70, 5.31, Rotation2d.fromDegrees(0));
+    public static final Pose2d redBottomLeft2 = new Pose2d(9.20, 2.80, Rotation2d.fromDegrees(0));
+
+
+    public static final Pose2d redTopLeft3 = new Pose2d(11.21, 2.55, Rotation2d.fromDegrees(0));
+    public static final Pose2d redBottomRight3 = new Pose2d(14.26, 2.55, Rotation2d.fromDegrees(0));
+    public static final Pose2d redTopRight3 = new Pose2d(13.35, 3.66, Rotation2d.fromDegrees(0));
+    public static final Pose2d redBottomLeft3 = new Pose2d(12.32, 1.22, Rotation2d.fromDegrees(0));
   }
   
   
--- src/main/java/frc/robot/commands/AutoHang.java
+++ src/main/java/frc/robot/commands/AutoHang.java
@@ -1,12 +1,17 @@
 package frc.robot.commands;
 
+import java.util.Optional;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.Constants;
 
 public class AutoHang {
 
 
     // basically tbe domainn and range for x and y values of the command
-    public static boolean isPoseInRectangle(Pose2d position, Pose2d topLeft, Pose2d bottomRight) {
+    public static boolean isPoseInRectangle(Pose2d position, Pose2d topLeft, Pose2d bottomRight, Pose2d topRight, Pose2d bottomLeft) {
         boolean betweenX = position.getX() > topLeft.getX() && position.getX() < bottomRight.getX();
         boolean betweenY = position.getY() < topLeft.getY() && position.getY() < bottomRight.getY();
 
@@ -16,5 +21,75 @@ public class AutoHang {
         else {
             return false;
         }
+    }
+
+    // write a method that returns an angle based on the position it's in 
+    // if the robot is in allaicne red but the robot is in a blue thingy, then just return 0 or somtehing
+    public static double getAngle(Pose2d robotPose) {
+        Alliance alliance = getTargetPose();
+
+        if (alliance == Alliance.Blue) {
+            if (isPoseInRectangle(robotPose, 
+                Constants.AutoHang.blueBottomLeft1,
+                Constants.AutoHang.blueBottomRight1,
+                Constants.AutoHang.blueTopRight1,
+                Constants.AutoHang.blueTopLeft1
+            )) {
+                return 120;
+            }
+            else if (isPoseInRectangle(robotPose, 
+                Constants.AutoHang.blueBottomLeft2,
+                Constants.AutoHang.blueBottomRight2,
+                Constants.AutoHang.blueTopRight2,
+                Constants.AutoHang.blueTopLeft2
+            )) {
+                return 0;
+            }
+            else if (isPoseInRectangle(robotPose, 
+                Constants.AutoHang.blueBottomLeft3,
+                Constants.AutoHang.blueBottomRight3,
+                Constants.AutoHang.blueTopRight3,
+                Constants.AutoHang.blueTopLeft3
+            )) {
+                return -120;
+            }
+        }
+        if (alliance == Alliance.Red) {
+            if (isPoseInRectangle(robotPose, 
+                Constants.AutoHang.redBottomLeft1,
+                Constants.AutoHang.redBottomRight1,
+                Constants.AutoHang.redTopRight1,
+                Constants.AutoHang.redTopLeft1
+            )) {
+                return 60;
+            }
+            else if (isPoseInRectangle(robotPose, 
+                Constants.AutoHang.redBottomLeft2,
+                Constants.AutoHang.redBottomRight2,
+                Constants.AutoHang.redTopRight2,
+                Constants.AutoHang.redTopLeft2
+            )) {
+                return -180;
+            }
+            else if (isPoseInRectangle(robotPose, 
+                Constants.AutoHang.redBottomLeft3,
+                Constants.AutoHang.redBottomRight3,
+                Constants.AutoHang.redTopRight3,
+                Constants.AutoHang.redTopLeft3
+            )) {
+                return -60;
+            }
+        }
+
+        return 0; 
+    }
+
+    public static Alliance getTargetPose() {
+        Optional<Alliance> optAlliance = DriverStation.getAlliance();
+
+        if (optAlliance.isEmpty()) return null;
+
+        Alliance alliance = optAlliance.get(); 
+        return alliance; 
     }
 }
